d733d8dc6329efa890bb51afe4dac8ea63c977d1
rayangendre <64104432+rayangendre@users.noreply.github.com>
2021-03-22 13:59:46 -0700

    Add files via upload

---
+++ calculator/BinaryExpression.java
@@ -0,0 +1,23 @@
+abstract class BinaryExpression implements Expression {
+
+    private final Expression lft;
+    private final Expression rht;
+    private final String operator;
+
+    public BinaryExpression(Expression lft, Expression rht, String operator) {
+        this.lft = lft;
+        this.rht = rht;
+        this.operator = operator;
+    }
+
+    @Override
+    public double evaluate(Bindings bindings) {
+        return _applyOperator(this.lft.evaluate(bindings), this.rht.evaluate(bindings));
+    }
+
+    public String toString(){
+        return "(" + lft + " " + operator + " " + rht + ")";
+    }
+
+    protected abstract double _applyOperator(double lft, double rht);
+}
---
+++ calculator/DivideExpression.java
@@ -0,0 +1,17 @@
+class DivideExpression
+   extends BinaryExpression
+
+{
+
+   public DivideExpression(final Expression lft, final Expression rht)
+   {
+      super(lft, rht, "/");
+   }
+
+   @Override
+   protected double _applyOperator(double lft, double rht) {
+      return lft / rht;
+   }
+
+}
+
